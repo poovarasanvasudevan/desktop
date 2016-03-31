f9992af3e6bab0ff657c503b85ffe97b3d5188a5
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-03-31 17:56:10 +0300

    Assign notif callback to identifier instead of tag

--- src/scripts/browser/components/native-notifier/impl-darwin.js
+++ src/scripts/browser/components/native-notifier/impl-darwin.js
@@ -91,7 +91,7 @@ class DarwinNativeNotifier extends BaseNativeNotifier {
 
     // Click callback
     if (onClick) {
-      this.on('notif-activated-' + tag, onClick);
+      this.on('notif-activated-' + identifier, onClick);
     }
 
     // Creation callback
