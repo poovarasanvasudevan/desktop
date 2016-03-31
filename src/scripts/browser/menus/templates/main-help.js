6188c9613d457417ffb21222095dd660381998bd
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-03-31 16:34:12 +0300

    Removes Help > Email Support item

--- src/scripts/browser/menus/templates/main-help.js
+++ src/scripts/browser/menus/templates/main-help.js
@@ -4,9 +4,6 @@ export default {
   label: '&Help',
   role: 'help',
   submenu: [{
-    label: '&Email Support',
-    click: $.openUrl('mailto:support@chatra.io')
-  }, {
     label: '&Visit our Website',
     click: $.openUrl('https://chatra.io/')
   }]
